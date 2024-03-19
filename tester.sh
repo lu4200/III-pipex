ae072624c2cf0f7a9b3cea8df24d1cc0fd65dab2
lumaret <lumaret@student.42.fr>
2024-03-19 19:57:04 +0100

    more tests

--- tester.sh
+++ tester.sh
@@ -74,7 +74,7 @@ else
     echo -e "wc + sort -n : \033[34m[CHECK LOGS]\033[0m \n"
 fi
 
-rm input4.txt output4.txt expected_output4.txt
+#rm input4.txt output4.txt expected_output4.txt
 
 ############################# SED TRUNC ############################################ 5
 
@@ -93,7 +93,64 @@ else
     echo -e "sed + trunc : \033[31m[FAIL]\033[0m \n"
 fi
 
-#rm input5.txt output5.txt expected_output5.txt
+rm input5.txt output5.txt expected_output5.txt
 
-##################################################################################### 6
+#################################### AWK SORT ########################################### 6
 
+# Création du fichier input.txt avec des données pour le test
+echo -e "pomme 1\nbanane 2\nananas 3" > input6.txt
+
+# Exécution de la commande avec pipex
+./pipex input6.txt "awk {print \$2, \$1}" "sort -k1" output6.txt
+
+# Exécution de la commande avec bash
+awk '{print $2, $1}' input6.txt | sort -k1 > expected_output6.txt
+
+# Comparaison des résultats
+if diff -q output6.txt expected_output6.txt > /dev/null; then
+    echo -e "awk + sort : \033[32m[OK]\033[0m \n"
+else
+    echo -e "awk + sort : \033[31m[FAIL]\033[0m \n"
+fi
+
+rm input6.txt output6.txt expected_output6.txt
+
+###################################### CUT UNIQ ####################################### 7
+
+# Création du fichier input.txt avec des données pour le test
+echo -e "a:x:1000:a\nb:x:1001:b\nc:x:1002:a" > input7.txt
+
+# Exécution de la commande avec pipex
+./pipex input7.txt "cut -d : -f3" "uniq" output7.txt
+
+# Exécution de la commande avec bash
+cut -d ':' -f3 input7.txt | uniq > expected_output7.txt
+
+# Comparaison des résultats
+if diff -q output7.txt expected_output7.txt > /dev/null; then
+    echo -e "cut + uniq : \033[32m[OK]\033[0m \n"
+else
+    echo -e "cut + uniq : \033[31m[FAIL]\033[0m \n"
+fi
+
+rm input7.txt output7.txt expected_output7.txt
+
+####################################### HEAD TAIL #################################### 8
+
+# Création du fichier input.txt avec des données pour le test
+seq 100 > input8.txt
+
+# Exécution de la commande avec pipex
+./pipex input8.txt "head -n 10" "tail -n 5" output8.txt
+
+# Exécution de la commande avec bash
+head -n 10 input8.txt | tail -n 5 > expected_output8.txt
+
+# Comparaison des résultats
+if diff -q output8.txt expected_output8.txt > /dev/null; then
+    echo -e "head + tail : \033[32m[OK]\033[0m \n"
+else
+    echo -e "head + tail : \033[31m[FAIL]\033[0m \n"
+fi
+
+rm input8.txt output8.txt expected_output8.txt
